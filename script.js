2b9ca16e0bb2a010d77514a59913b54716f90e8e
Leon Niederpruem <leon.niederpruem@ivx.com>
2021-08-24 16:40:34 +0200

    Remove liveCalc

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const equalsButton = document.querySelector("[data-equals]");
 const clearButton = document.querySelector("[data-clear]");
 const deleteButton = document.querySelector("[data-delete]");
 const inputNumberField = document.querySelector(".inputNumber");
-// const liveOperation = document.querySelector(".liveOperation");
 
 let inputNumber = inputNumberField.innerHTML;
 
